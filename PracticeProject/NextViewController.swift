0dd701f6411846e30e047ef922407efa448508fd
YongShunTu <a49980060@gmail.com>
2021-11-15 10:47:49 +0800

    text

---
+++ PracticeProject/NextViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NextViewController.swift
+//  PracticeProject
+//
+//  Created by 姜又寧 on 2021/11/14.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func dismissButtonClicked(_ sender: UIButton) {
+        self.dismiss(animated: true) {
+            print("success")
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
